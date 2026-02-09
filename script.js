a03f15851ec04914bc4ac0d4d1039232ff68cf2b
unknown <rabbymd55555@gmail.com>
2026-02-09 15:18:26 +0600

    added script js

---
+++ script.js
@@ -0,0 +1,15 @@
+// console.dir(document);
+// console.log(typeof document);
+// console.log(document.title);
+// console.log(document.location);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// document.title = "Play With DOM";
+
+// for (let element of document.all) {
+//   console.log(element);
+// }
+
+console.log(document.all[5]);
+console.log(document.images);
